b3100caa59a8199b5e242492e8e6c0ac2c536b8e
Ryan Kurte <ryankurte@gmail.com>
2019-07-02 13:42:08 +1200

    removed std from blocking impl

--- src/blocking.rs
+++ src/blocking.rs
@@ -1,14 +1,16 @@
 
-extern crate std;
-use blocking::std::*;
-use blocking::std::time::Duration;
+use core::time::Duration;
+
+extern crate embedded_hal;
+use blocking::embedded_hal::blocking::delay::DelayMs;
 
 use crate::{Transmit, Receive, Power};
 
 /// Blocking transmit function that uses thread_sleep internally to transmit a packet
-pub fn do_transmit<T, E>(radio: &mut T, data: &[u8], power: Option<i8>, poll_interval: Duration) -> Result<(), E> 
+pub fn do_transmit<T, D, E>(radio: &mut T, data: &[u8], power: Option<i8>, mut delay: D, poll_interval: Duration) -> Result<(), E> 
 where
-    T: Transmit<Error=E> + Power<Error=E>
+    T: Transmit<Error=E> + Power<Error=E>,
+    D: DelayMs<u32>,
 {
     // Set output power if specified
     if let Some(p) = power {
@@ -21,16 +23,17 @@ where
             debug!("Send complete");
             break;
         }
-        std::thread::sleep(poll_interval);
+        delay.delay_ms(poll_interval.as_millis() as u32);
     }
 
     Ok(())
 }
 
-pub fn do_receive<T, I, E>(radio: &mut T, mut buff: &mut [u8], mut info: &mut I, poll_interval: Duration) -> Result<usize, E> 
+pub fn do_receive<T, I, D, E>(radio: &mut T, mut buff: &mut [u8], mut info: &mut I, mut delay: D, poll_interval: Duration) -> Result<usize, E> 
 where
     T: Receive<Info=I, Error=E>,
-    I: std::fmt::Debug,
+    I: core::fmt::Debug,
+    D: DelayMs<u32>,
 {
     // Start receive mode
     radio.start_receive()?;
@@ -38,15 +41,9 @@ where
     loop {
         if radio.check_receive(true)? {
             let n = radio.get_received(&mut info, &mut buff)?;
-
-            match std::str::from_utf8(&buff[0..n as usize]) {
-                Ok(s) => info!("Received: '{}' info: {:?}", s, info),
-                Err(_) => info!("Received: '{:?}' info: {:?}", &buff[0..n as usize], info),
-            }
-            
             return Ok(n)
         }
-        std::thread::sleep(poll_interval);
+        delay.delay_ms(poll_interval.as_millis() as u32);
     }
 }
 
